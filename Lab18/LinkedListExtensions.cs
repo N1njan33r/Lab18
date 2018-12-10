5da84cb46fa2265d8ab444f5ef504f21a902e503
Alexander Inman <fujiwara06@gmail.com>
2018-12-09 22:01:57 -0500

    Added original files

---
+++ Lab18/LinkedListExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Lab18
+{
+    public static class LinkedListExtensions
+    {
+        public static bool RemoveAt<T>(this LinkedList<T> list, int index)
+        {
+            if (index > list.Count - 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static void PrintReverse<T>(this LinkedList<T> list)
+        {
+            LinkedList<T> temp = new LinkedList<T>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                temp.AddLast(list.Last);
+                list.RemoveLast();
+            }
+        }
+
+        public static bool InsertAt<T>(this LinkedList<T> list, int index)
+        {
+            return true;
+        }
+    }
+}
---
+++ Lab18/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] blahblah = { 1,2,3,2,2,4,5,5,7,8,4,4,1,0,10 };
+            //LinkedListExtensions.PrintReverse<int>(blahblah.ToList<int>);
+        }
+    }
+}
